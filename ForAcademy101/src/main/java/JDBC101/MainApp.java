db6b5ed6e11e56967968623786ea6a299fe51181
Akram <akram.iguernait@gmail.com>
2021-12-16 11:54:58 +0100

    still needs update

--- ForAcademy101/src/main/java/JDBC101/MainApp.java
+++ ForAcademy101/src/main/java/JDBC101/MainApp.java
@@ -7,16 +7,10 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         String jdbcUrl = "jdbc:postgresql://localhost:5432/FA101";
         String username = "postgres";
         String password = "hello";
 
-=======
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/test101";
-        String username = "postgres";
-        String password = "5687";
->>>>>>> a9f136ec0892e4826526e351867f9eb21db3c254
 
         User user = new User();
         user.setFirst_name("anouar");
