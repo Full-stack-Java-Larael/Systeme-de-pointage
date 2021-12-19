9af2231d0abd151a6cfd57578e99310025830507
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:53:22 +0100

    Teacher Service interface

--- ForAcademy101/src/main/java/JDBC101/service/teacherService.java
+++ ForAcademy101/src/main/java/JDBC101/service/teacherService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface teacherService {
+package JDBC101.service;
+
+import JDBC101.model.Teacher;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface teacherService {
+    public void addTeacher(Teacher Teacher);
+    public Optional<Teacher> fetchTeacherById(int id_teacher);
+    public void deleteTeacherById(Teacher id_teacher);
+    public void updateTeacherPasswordById(String newPassword, String[] id_teacher);
+    public List<Teacher> getAllTeachers();
 }
