8918b4b1f6b994c9cb2cb03394ff5dc7ad51c66e
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:51:42 +0100

    Secretary Service interface

--- ForAcademy101/src/main/java/JDBC101/service/secretaryService.java
+++ ForAcademy101/src/main/java/JDBC101/service/secretaryService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface secretaryService {
+package JDBC101.service;
+
+import JDBC101.model.Secretary;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface secretaryService {
+    public void addSecretary(Secretary Secretary);
+    public Optional<Secretary> fetchSecretaryById(int id_secretary);
+    public void deleteSecretaryById(Secretary id_secretary);
+    public void updateSecretaryPasswordById(String newPassword, String[] id_secretary);
+    public List<Secretary> getAllSecretarys();
 }
