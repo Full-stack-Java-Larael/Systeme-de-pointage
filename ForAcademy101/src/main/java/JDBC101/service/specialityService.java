8ba2c90ed52271a41277914fa96034b002679a4d
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:52:02 +0100

    Speciality Service interface

--- ForAcademy101/src/main/java/JDBC101/service/specialityService.java
+++ ForAcademy101/src/main/java/JDBC101/service/specialityService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface specialityService {
+package JDBC101.service;
+
+import JDBC101.model.Speciality;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface specialityService {
+    public void addSpeciality(Speciality Speciality);
+    public Optional<Speciality> fetchSpecialityById(int id_Speciality);
+    public void deleteSpecialityById(Speciality id_Speciality);
+    public void updateSpecialityPasswordById(String newPassword, String[] id_Speciality);
+    public List<Speciality> getAllSpecialitys();
 }
