ab6fd1e65844f9f5011ac8e7bb5da1213cda3f64
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:49:13 +0100

    ClassRoom Service interface

--- ForAcademy101/src/main/java/JDBC101/service/classRoomService.java
+++ ForAcademy101/src/main/java/JDBC101/service/classRoomService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface classRoomService {
+package JDBC101.service;
+
+import JDBC101.model.ClassRoom;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface classRoomService {
+    public void addClassRoom(ClassRoom ClassRoom);
+    public Optional<ClassRoom> fetchClassRoomById(int id_classRoom);
+    public void deleteClassRoomById(ClassRoom id_classRoom);
+    public void updateClassRoomPasswordById(String newPassword, String[] id_classRoom);
+    public List<ClassRoom> getAllClassRooms();
 }
