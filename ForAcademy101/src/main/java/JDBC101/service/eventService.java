eed578f7a036789b7198b44b6117fed5d99227cf
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:49:49 +0100

    Event Service interface

--- ForAcademy101/src/main/java/JDBC101/service/eventService.java
+++ ForAcademy101/src/main/java/JDBC101/service/eventService.java
@@ -1,2 +1,15 @@
-package JDBC101.service;public interface eventService {
+package JDBC101.service;
+
+import JDBC101.model.Event;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface eventService {
+
+    public void addEvent(Event Event);
+    public Optional<Event> fetchEventById(int id_event);
+    public void deleteEventById(Event id_event);
+    public void updateEventPasswordById(String newPassword, String[] id_event);
+    public List<Event> getAllEvents();
 }
