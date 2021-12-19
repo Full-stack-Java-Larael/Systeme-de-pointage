760c00fa53e739420ea856c4397b3103173914ef
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:47:38 +0100

    Admin Service interface

--- ForAcademy101/src/main/java/JDBC101/service/adminService.java
+++ ForAcademy101/src/main/java/JDBC101/service/adminService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface adminService {
+package JDBC101.service;
+
+import JDBC101.model.Admin;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface adminService {
+    public void addAdmin(Admin Admin);
+    public Optional<Admin> fetchAdminById(int id_admin);
+    public void deleteAdminById(Admin id_admin);
+    public void updateAdminPasswordById(String newPassword, String[] id_admin);
+    public List<Admin> getAllAdmins();
 }
