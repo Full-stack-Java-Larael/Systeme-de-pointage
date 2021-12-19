dedbe4712a67ff2d32dad3c3b308ebf5ece6c0bd
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:50:12 +0100

    Manager Service interface

--- ForAcademy101/src/main/java/JDBC101/service/managerService.java
+++ ForAcademy101/src/main/java/JDBC101/service/managerService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface managerService {
+package JDBC101.service;
+
+import JDBC101.model.Manager;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface managerService {
+    public void addManager(Manager Manager);
+    public Optional<Manager> fetchManagerById(int id_manager);
+    public void deleteManagerById(Manager id_manager);
+    public void updateManagerPasswordById(String newPassword, String[] id_manager);
+    public List<Manager> getAllManagers();
 }
