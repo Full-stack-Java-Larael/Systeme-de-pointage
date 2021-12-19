8bc0553ea6244984d1f0ca9d4cc48f3f80d62c9e
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:48:39 +0100

    Attendance Service interface

--- ForAcademy101/src/main/java/JDBC101/service/attendanceService.java
+++ ForAcademy101/src/main/java/JDBC101/service/attendanceService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface attendanceService {
+package JDBC101.service;
+
+import JDBC101.model.Attendance;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface attendanceService {
+    public void addAttendance(Attendance Attendance);
+    public Optional<Attendance> fetchAttendanceById(int id_attendance);
+    public void deleteAttendanceById(Attendance id_attendance);
+    public void updateAttendancePasswordById(String newPassword, String[] id_attendance);
+    public List<Attendance> getAllAttendances();
 }
