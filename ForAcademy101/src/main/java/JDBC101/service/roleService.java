6229135beeededbcc7e10c9e689f022bda1a5320
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:51:16 +0100

    Role Service interface

--- ForAcademy101/src/main/java/JDBC101/service/roleService.java
+++ ForAcademy101/src/main/java/JDBC101/service/roleService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface roleService {
+package JDBC101.service;
+
+import JDBC101.model.Role;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface roleService {
+    public void addRole(Role Role);
+    public Optional<Role> fetchRoleById(int id_role);
+    public void deleteRoleById(Role id_role);
+    public void updateRolePasswordById(String newPassword, String[] id_role);
+    public List<Role> getAllRoles();
 }
