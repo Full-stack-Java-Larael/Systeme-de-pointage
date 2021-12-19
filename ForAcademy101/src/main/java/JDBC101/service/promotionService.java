2e96bebcc0099ef3305668fbdf026263a9f05292
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:50:42 +0100

    Promotion Service interface

--- ForAcademy101/src/main/java/JDBC101/service/promotionService.java
+++ ForAcademy101/src/main/java/JDBC101/service/promotionService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface promotionService {
+package JDBC101.service;
+
+import JDBC101.model.Promotion;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface promotionService {
+    public void addPromotion(Promotion Promotion);
+    public Optional<Promotion> fetchPromotionById(int id_promotion);
+    public void deletePromotionById(Promotion id_promotion);
+    public void updatePromotionPasswordById(String newPassword, String[] id_promotion);
+    public List<Promotion> getAllPromotions();
 }
