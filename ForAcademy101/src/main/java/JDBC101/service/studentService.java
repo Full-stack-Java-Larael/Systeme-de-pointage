ceae2244474e0bfa999dc717c738df2a3ca10d40
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-19 22:52:22 +0100

    Speciality Service interface

--- ForAcademy101/src/main/java/JDBC101/service/studentService.java
+++ ForAcademy101/src/main/java/JDBC101/service/studentService.java
@@ -1,2 +1,14 @@
-package JDBC101.service;public interface studentService {
+package JDBC101.service;
+
+import JDBC101.model.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface studentService {
+    public void addStudent(Student Student);
+    public Optional<Student> fetchStudentById(int id_student);
+    public void deleteStudentById(Student id_student);
+    public void updateStudentPasswordById(String newPassword, String[] id_student);
+    public List<Student> getAllStudents();
 }
