f5b606f4810990e0f477713bb25b496ecf28ced7
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-12-22 11:39:12 +0100

    ignoring JDBC Factory

---
+++ ForAcademy101/src/test/java/JDBC101/serviceImpl/AddressServiceImplTest.java
@@ -0,0 +1,28 @@
+package JDBC101.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressServiceImplTest {
+
+    @Test
+    void addAddress() {
+    }
+
+    @Test
+    void fetchAddressById() {
+    }
+
+    @Test
+    void deleteAddressById() {
+    }
+
+    @Test
+    void updateAddressPasswordById() {
+    }
+
+    @Test
+    void getAllAddresss() {
+    }
+}
---
+++ ForAcademy101/src/test/java/JDBC101/serviceImpl/AdminServiceImplTest.java
@@ -0,0 +1,7 @@
+package JDBC101.serviceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceImplTest {
+
+}
