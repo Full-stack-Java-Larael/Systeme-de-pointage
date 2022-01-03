b4509702a6d318caf5e724f36249fd30f260b498
Diouani <73253036+Diouani@users.noreply.github.com>
2022-01-03 16:07:57 +0100

    resolution conflict

--- ForAcademy101/src/main/java/JDBC101/serviceImpl/AddressServiceImpl.java
+++ ForAcademy101/src/main/java/JDBC101/serviceImpl/AddressServiceImpl.java
@@ -1,5 +1,6 @@
 package JDBC101.serviceImpl;
 import JDBC101.model.Address;
+import JDBC101.model.Admin;
 import JDBC101.service.*;
 import JDBC101.daoImpl.*;
 
@@ -22,6 +23,7 @@ public class AddressServiceImpl implements addressService  {
     @Override
     public void deleteAddressById(Address address) {
 
+
         addressDao.deleteAddress(address);
 
     }
@@ -29,9 +31,7 @@ public class AddressServiceImpl implements addressService  {
     @Override
     public void updateAddress( Address address) {
 
-
-
-        addressDao.updateAddress(address);
+addressDao.deleteAddress(address);
     }
 
 
